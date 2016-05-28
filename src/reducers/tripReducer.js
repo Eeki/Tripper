4100a4edbb17396c4077fb7d4a9974a50871eac3
Rainer Kujala <rainer.kujala@gmail.com>
2016-05-28 19:39:06 +0300

    data models for trips

--- src/reducers/tripReducer.js
+++ src/reducers/tripReducer.js
@@ -3,60 +3,133 @@ import { OPTIMIZE_TRIP } from '../actions/const'
 const initialState = [
   {
     time: 300,
-    journey: [
+    start: {
+      type: HOTEL,
+      id: 2
+    },
+    days: [ // a day is a list of itienaries
       {
-        start: {
-          type: HOTEL,
-          id: 2
-        },
-        days: [
-          itineraries: [
-            {
-              goal: {
-                type: ATTRACTION,
-                id: 21
-              }
-              legs: [
-                {
-                  startTime: 1464426014000,
-                  endTime: 1464426370000,
-                  geometry: "_tlnJm`fwC_@NOF@L@H@PFfADhC@TT|DBh@?BFdAJvADp@Dt@HtA?DDr@@H@FJrB@D???B?DBVB\\D|@FfATI"
-                },
-                {
-                  startTime: 1464426014000,
-                  endTime: 1464426370000,
-                  geometry: "_tlnJm`fwC_@NOF@L@H@PFfADhC@TT|DBh@?BFdAJvADp@Dt@HtA?DDr@@H@FJrB@D???B?DBVB\\D|@FfATI"
-                }
-              ]
+        itienaries: [
+          {
+            goal: {
+              type: ATTRACTION,
+              id: 21
             },
+            legs: [
+              {
+                startTime: 1464426014000,
+                endTime: 1464426370000,
+                geometry: "_tlnJm`fwC_@NOF@L@H@PFfADhC@TT|DBh@?BFdAJvADp@Dt@HtA?DDr@@H@FJrB@D???B?DBVB\\D|@FfATI"
+                transportMode: "WALK"
+              },
+              {
+                startTime: 1464426371000,
+                endTime: 1464426646000,
+                mode: "RAIL",
+                geometry: "splnJgydwC|iDyp@"
+              }
+            ]
+          },
+          {
             goal: {
               type: LUNCH,
               time: 45
             }
-          ]
+          }
+        ]
+      }, // end day 1
+      { // start day 2
+        itienaries: [ // start day 2
+          { // first itienary
+            goal: {
+              type: ATTRACTION,
+              id: 21
+            },
+            legs: [
+              {
+                startTime: 1464430457000,
+                endTime: 1464430799000,
+                mode: "WALK",
+                geometry: "eepnJeihvCP_AHJBOPRGb@CNCV?N@TBTmCr@uAf@VhFNrC@X?LEJXlF"
+              },
+              {
+                startTime: 1464430800000,
+                endTime: 1464431160000,
+                mode: "RAIL",
+                geometry: "eipnJqngvCoHazDfFatN"
+              },
+              {
+                startTime: 1464431160000,
+                endTime: 1464431240000,
+                mode: "WALK",
+                geometry: "mkpnJu~|vCU|@DF]tAYc@"
+              },
+              {
+                startTime: 1464431580000,
+                endTime: 1464432720000,
+                mode: "BUS",
+                geometry: "{mpnJiz|vCW_@KGYc@_DuFGMwCeF]c@][s@g@a@UwBa@wCm@iBa@i@W{Au@e@YGGOQEGMW@K@UCSEKGGICEBWSi@_@i@YUWi@u@e@_Ay@kBi@wAg@eBCc@yAmI[yBUeBw@kGAKC]DKBQ?M?QEOCKIIGAIBG]AEAIWoB]gCAGKu@Os@UuAOq@Sy@qD}Wk@cEKw@aBuLe@sDE_@mAcIMy@GYGYAMMc@c@uAAGGc@GSY{@IWUm@_AyBMYKMQIe@}@c@}@wCeGIS{@gBYk@oAkCKWQg@Ig@Ga@Gg@Ei@Ci@CsAAaBCwHCyC?Q?q@?a@B[?y@?O?S@U@A@A?C@A@C?A?CJSFKb@e@HGp@u@hAkAnC{Cf@k@HIj@s@LQHOf@{@HMLUJSFQTq@LWVm@P[DIDMNi@\\gBFWrAuGH]Hc@XoAJi@Jk@Ju@J_AH}@JkADmABc@@]BgA@uABuB@iA?_@AmAAqACoACiAC{ACi@EmAOuCKcBO{AYsC]iDCKASe@eDU}Bo@mFIq@GkACSg@uDEYKo@]yBCQOy@_@wAeAiHiAqIAUM{@CMC[AM@EBI@M?IAMCKCGEEECE@GBEFAFCL?L?JBJBHBDBDF@DABCDG@EBIFMPQl@y@LQP_@HO@QBGDGN[pBiELSLWHOHK~CkFHKHMFKz@wABGLQj@w@FCFKDEPSDBFADEBE@I@I?INk@Rm@He@BGJWHS|AsDRO`A_CB_@Xm@h@kAMi@EQMWMOKGO?KBMHQTGDOLG@KCMOUe@Wm@M]]{@Se@c@cAeCaGyCmHMYM]]y@Ma@Mc@Kg@OeAMgAIu@KyAEeAEaACoAAY?qA@mABaBHmCToKJqEBiB@qB@_B?aCIsLKuOAeAAwDKqMEyC?{A@uA@yADyAJwCRkC?Ed@yET}AZkBVcARu@Rs@L[JYTk@Vo@^y@zDgIjA_CBGHQHSFOGNIRHSFO`@gAr@mCTaAZsA\\cBPcAFa@N}AL{@Ry@~@kDdBgGNi@Pm@BQ@M@O?_@?[?SCe@Ec@O[uAeBqA}AWa@Uk@Ke@C_@?W@a@@QNoBHoA"
+              },
+              {
+                startTime: 1464432721000,
+                endTime: 1464432922000,
+                mode: "WALK",
+                geometry: "cbrnJwetwCFq@Dm@GKKOAAKEA?a@yBUISk@GAGAY@E@EBA?IDMHAAQCWK][I_@"
+              }
+            ] // end legs
+          } // end itienary
+        ]
+      }
+    ] // end days
+  }, // end hotel, or trip
+  {
+    time: 150,
+    start: {
+      type: HOTEL,
+      id: 3
+    },
+    days: [
+      {
+        itienaries: [
+          { // itienary
+            goal: {
+              type: ATTRACTION,
+              id: 42
+            }
+          },
+          {
+            goal: {
+              type: HOTEL,
+              id: 66
+            }
+          },
+          {
+            goal: {
+              type: LUNCH,
+              time: 45
+            }
+          }
+        ]
+      },
+      {
+        itienaries: [
+          {
+            goal: {
+              type: ATTRACTION,
+              id: 32
+            },
+          },
+          {
+            goal: {
+              type: HOTEL,
+              id: 66
+            }
+          }
         ]
       }
     ]
-  },
-  {
-    time: 150,
-    journey: {
-      start: {
-        type: HOTEL,
-        id: 3
-      },
-      days: [
-        itineraries: [
-          goal: {
-            type: ATTRACTION,
-            id: 42
-          }
-          // legs is undefined
-        ]
-      ]
-    }
   }
-]
+];
 
 export const tripReducer = (state = initialState, action) {
   switch (action.type) {
