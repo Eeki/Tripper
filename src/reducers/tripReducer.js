7ad840bea0c227291445ce20cf3da3c6342f5beb
Rainer Kujala <rainer.kujala@gmail.com>
2016-05-28 22:18:58 +0300

    Restructure trips model to match the graphQL response

--- src/reducers/tripReducer.js
+++ src/reducers/tripReducer.js
@@ -3,7 +3,7 @@ import { OPTIMIZE_START } from '../actions/const'
 // a list of journeys, one should contain fully optimized results (at some point, at least)
 const initialState = [
   {
-    time: 300,
+    duration: 300,
     start: {
       type: HOTEL,
       id: 2
@@ -21,21 +21,24 @@ const initialState = [
                 startTime: 1464426014000,
                 endTime: 1464426370000,
                 geometry: "_tlnJm`fwC_@NOF@L@H@PFfADhC@TT|DBh@?BFdAJvADp@Dt@HtA?DDr@@H@FJrB@D???B?DBVB\\D|@FfATI"
-                transportMode: "WALK"
+                mode: "WALK"
               },
               {
                 startTime: 1464426371000,
-                endTime: 1464426646000,
+                endTime: 1464426014000,
                 mode: "RAIL",
-                geometry: "splnJgydwC|iDyp@"
+                legGeometry: {
+                  points: "splnJgydwC|iDyp@"
+                }
               }
-            ]
+            ],
+            duration: 1464426014000-1464426014000
           },
           {
             goal: {
               type: LUNCH,
-              time: 45
             }
+            duration: 100
           }
         ]
       }, // end day 1
@@ -51,40 +54,51 @@ const initialState = [
                 startTime: 1464430457000,
                 endTime: 1464430799000,
                 mode: "WALK",
-                geometry: "eepnJeihvCP_AHJBOPRGb@CNCV?N@TBTmCr@uAf@VhFNrC@X?LEJXlF"
+                legGeometry: {
+                  points: "eepnJeihvCP_AHJBOPRGb@CNCV?N@TBTmCr@uAf@VhFNrC@X?LEJXlF"
+                }
               },
               {
                 startTime: 1464430800000,
                 endTime: 1464431160000,
                 mode: "RAIL",
-                geometry: "eipnJqngvCoHazDfFatN"
+                legGeometry: {
+                  points: "eipnJqngvCoHazDfFatN"
+                }
               },
               {
                 startTime: 1464431160000,
                 endTime: 1464431240000,
                 mode: "WALK",
-                geometry: "mkpnJu~|vCU|@DF]tAYc@"
+                legGeometry: {
+                  points: "mkpnJu~|vCU|@DF]tAYc@"
+                }
               },
               {
                 startTime: 1464431580000,
                 endTime: 1464432720000,
                 mode: "BUS",
-                geometry: "{mpnJiz|vCW_@KGYc@_DuFGMwCeF]c@][s@g@a@UwBa@wCm@iBa@i@W{Au@e@YGGOQEGMW@K@UCSEKGGICEBWSi@_@i@YUWi@u@e@_Ay@kBi@wAg@eBCc@yAmI[yBUeBw@kGAKC]DKBQ?M?QEOCKIIGAIBG]AEAIWoB]gCAGKu@Os@UuAOq@Sy@qD}Wk@cEKw@aBuLe@sDE_@mAcIMy@GYGYAMMc@c@uAAGGc@GSY{@IWUm@_AyBMYKMQIe@}@c@}@wCeGIS{@gBYk@oAkCKWQg@Ig@Ga@Gg@Ei@Ci@CsAAaBCwHCyC?Q?q@?a@B[?y@?O?S@U@A@A?C@A@C?A?CJSFKb@e@HGp@u@hAkAnC{Cf@k@HIj@s@LQHOf@{@HMLUJSFQTq@LWVm@P[DIDMNi@\\gBFWrAuGH]Hc@XoAJi@Jk@Ju@J_AH}@JkADmABc@@]BgA@uABuB@iA?_@AmAAqACoACiAC{ACi@EmAOuCKcBO{AYsC]iDCKASe@eDU}Bo@mFIq@GkACSg@uDEYKo@]yBCQOy@_@wAeAiHiAqIAUM{@CMC[AM@EBI@M?IAMCKCGEEECE@GBEFAFCL?L?JBJBHBDBDF@DABCDG@EBIFMPQl@y@LQP_@HO@QBGDGN[pBiELSLWHOHK~CkFHKHMFKz@wABGLQj@w@FCFKDEPSDBFADEBE@I@I?INk@Rm@He@BGJWHS|AsDRO`A_CB_@Xm@h@kAMi@EQMWMOKGO?KBMHQTGDOLG@KCMOUe@Wm@M]]{@Se@c@cAeCaGyCmHMYM]]y@Ma@Mc@Kg@OeAMgAIu@KyAEeAEaACoAAY?qA@mABaBHmCToKJqEBiB@qB@_B?aCIsLKuOAeAAwDKqMEyC?{A@uA@yADyAJwCRkC?Ed@yET}AZkBVcARu@Rs@L[JYTk@Vo@^y@zDgIjA_CBGHQHSFOGNIRHSFO`@gAr@mCTaAZsA\\cBPcAFa@N}AL{@Ry@~@kDdBgGNi@Pm@BQ@M@O?_@?[?SCe@Ec@O[uAeBqA}AWa@Uk@Ke@C_@?W@a@@QNoBHoA"
+                legGeometry: {
+                  points: "{mpnJiz|vCW_@KGYc@_DuFGMwCeF]c@][s@g@a@UwBa@wCm@iBa@i@W{Au@e@YGGOQEGMW@K@UCSEKGGICEBWSi@_@i@YUWi@u@e@_Ay@kBi@wAg@eBCc@yAmI[yBUeBw@kGAKC]DKBQ?M?QEOCKIIGAIBG]AEAIWoB]gCAGKu@Os@UuAOq@Sy@qD}Wk@cEKw@aBuLe@sDE_@mAcIMy@GYGYAMMc@c@uAAGGc@GSY{@IWUm@_AyBMYKMQIe@}@c@}@wCeGIS{@gBYk@oAkCKWQg@Ig@Ga@Gg@Ei@Ci@CsAAaBCwHCyC?Q?q@?a@B[?y@?O?S@U@A@A?C@A@C?A?CJSFKb@e@HGp@u@hAkAnC{Cf@k@HIj@s@LQHOf@{@HMLUJSFQTq@LWVm@P[DIDMNi@\\gBFWrAuGH]Hc@XoAJi@Jk@Ju@J_AH}@JkADmABc@@]BgA@uABuB@iA?_@AmAAqACoACiAC{ACi@EmAOuCKcBO{AYsC]iDCKASe@eDU}Bo@mFIq@GkACSg@uDEYKo@]yBCQOy@_@wAeAiHiAqIAUM{@CMC[AM@EBI@M?IAMCKCGEEECE@GBEFAFCL?L?JBJBHBDBDF@DABCDG@EBIFMPQl@y@LQP_@HO@QBGDGN[pBiELSLWHOHK~CkFHKHMFKz@wABGLQj@w@FCFKDEPSDBFADEBE@I@I?INk@Rm@He@BGJWHS|AsDRO`A_CB_@Xm@h@kAMi@EQMWMOKGO?KBMHQTGDOLG@KCMOUe@Wm@M]]{@Se@c@cAeCaGyCmHMYM]]y@Ma@Mc@Kg@OeAMgAIu@KyAEeAEaACoAAY?qA@mABaBHmCToKJqEBiB@qB@_B?aCIsLKuOAeAAwDKqMEyC?{A@uA@yADyAJwCRkC?Ed@yET}AZkBVcARu@Rs@L[JYTk@Vo@^y@zDgIjA_CBGHQHSFOGNIRHSFO`@gAr@mCTaAZsA\\cBPcAFa@N}AL{@Ry@~@kDdBgGNi@Pm@BQ@M@O?_@?[?SCe@Ec@O[uAeBqA}AWa@Uk@Ke@C_@?W@a@@QNoBHoA"
+                }
               },
               {
                 startTime: 1464432721000,
                 endTime: 1464432922000,
                 mode: "WALK",
-                geometry: "cbrnJwetwCFq@Dm@GKKOAAKEA?a@yBUISk@GAGAY@E@EBA?IDMHAAQCWK][I_@"
+                legGeometry: {
+                  points: "cbrnJwetwCFq@Dm@GKKOAAKEA?a@yBUISk@GAGAY@E@EBA?IDMHAAQCWK][I_@"
+                }
               }
-            ] // end legs
+            ], // end legs
+            duration: 1464432922000-1464430457000
           } // end itienary
         ]
       }
     ] // end days
   }, // end hotel, or trip
   {
-    time: 150,
+    duration: 150,
     start: {
       type: HOTEL,
       id: 3
@@ -96,19 +110,22 @@ const initialState = [
             goal: {
               type: ATTRACTION,
               id: 42
-            }
+            },
+            duration: 11
           },
           {
             goal: {
               type: HOTEL,
               id: 66
-            }
+            },
+            duration: 10
           },
           {
             goal: {
               type: LUNCH,
               time: 45
-            }
+            },
+            duration: 10
           }
         ]
       },
@@ -119,12 +136,14 @@ const initialState = [
               type: ATTRACTION,
               id: 32
             },
+            duration: 10
           },
           {
             goal: {
               type: HOTEL,
               id: 66
-            }
+            },
+            duration: 10
           }
         ]
       }
@@ -132,7 +151,8 @@ const initialState = [
   }
 ];
 
-export const tripReducer = (state = initialState, action) {
+
+export const tripReducer = (state=initialState, action) {
   switch (action.type) {
     default:
       return state;
